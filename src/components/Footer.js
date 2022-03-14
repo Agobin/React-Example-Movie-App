064a8ec78b087af471c86dd2c9ffdc11f388b79d
Agobin Folefac <afolefac@outlook.com>
2022-03-14 15:15:10 -0300

    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,9 @@
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer-text">Copyright {new Date().getFullYear()}</div>
+      <div className="footer-text">
+        Copyright {new Date().getFullYear()} by Agobin Folefac
+      </div>
     </div>
   );
 };
